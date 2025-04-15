322c3ff5d09d5290053ba0eb56cd5203e0d0a39b
Faiz Ahmad <faizahmad43430@gmail.com>
2025-04-16 00:28:18 +0530

    bug/ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,14 +12,14 @@ const Navbar = () => {
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='lg:text-2xl font-bold text-emerald-400 items-center space-x-2 flex text-xl'>
+					<Link to='/' className='lg:text-2xl font-bold text-emerald-400 items-center space-x-2 flex text-lg'>
 						E-Commerce
 					</Link>
 
 					<nav className='flex flex-wrap items-center gap-4'>
 						<Link
 							to={"/"}
-							className='text-gray-300 hover:text-emerald-400 transition duration-300 
+							className='text-gray-300 hover:text-emerald-400 transition duration-300 hidden md:block
 					 ease-in-out'
 						>
 							Home
